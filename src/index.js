53011e98d42d7ae4acf0ef11d7b3e393ebb0bfa7
IliyaKz <iliya-kuznetsov@mail.ru>
2022-02-07 01:10:40 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = [];
+  let bracketsArr = {};
+  bracketsConfig.forEach((item) => {
+    bracketsArr[item[0]] = item[1];
+  })
+  for (let i = 0; i < str.length; i++) {
+    if (arr.length === 0) {
+      arr.push(str[i]);
+    }
+    else { 
+      if (str[i] === bracketsArr[arr[arr.length - 1]]) {
+        arr.pop();
+      }
+      else {
+        arr.push(str[i]);
+      }
+    }
+  }
+  return (arr.length === 0) ? true : false;
+  }
